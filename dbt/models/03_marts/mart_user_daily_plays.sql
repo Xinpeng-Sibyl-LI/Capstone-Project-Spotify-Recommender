ffb902171a9fbc8ca88d9fd85104d7d71ed1cd79
Xinpeng-Sibyl-LI <xinpeng.xpli@gmail.com>
2025-07-19 11:36:38 +0700

    Update mart model

--- dbt/models/03_marts/mart_user_daily_plays.sql
+++ dbt/models/03_marts/mart_user_daily_plays.sql
@@ -1,4 +1,9 @@
-{{ config(materialized='table') }}
+{{ config(
+    materialized='incremental',
+    unique_key='track_id',
+    on_schema_change='fail',
+    incremental_strategy='merge'
+) }}
 
 with tracks_base as (
     select *
